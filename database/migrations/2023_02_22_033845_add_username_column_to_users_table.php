3ebc0720fb0a75eb76a0c7608e80edf73f63e78c
NattapongLam <it.9nat@gmail.com>
2023-02-22 10:53:02 +0700

    ok

---
+++ database/migrations/2023_02_22_033845_add_username_column_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsernameColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex('users_email_unique');
+            $table->string('email')->nullable()->change();
+            $table->string('username')->after('email_verified_at')->unique();
+            $table->string('avatar')->nullable()->comment('รูป')->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->unique()->change();;
+            $table->dropColumn('username');
+            $table->dropColumn('avatar');
+        });
+    }
+}
