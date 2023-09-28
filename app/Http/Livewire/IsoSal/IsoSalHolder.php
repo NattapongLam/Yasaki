ba5059324aa30bc4bf882b2b6ed559b8d384c96f
NattapongLam <it.9nat@gmail.com>
2023-09-28 09:35:05 +0700

    ok

--- app/Http/Livewire/IsoSal/IsoSalHolder.php
+++ app/Http/Livewire/IsoSal/IsoSalHolder.php
@@ -8,11 +8,16 @@ use Illuminate\Support\Facades\DB;
 class IsoSalHolder extends Component
 {
     public $holder = [];
+    public $policy = [];
 
     public function render()
     {
         $this->holder = DB::table('vw_iso_holderperson')
         ->where('emp_department_refcode','SAL')
+        ->get();
+        $this->policy = DB::table('iso_policy_lsits')
+        ->where('pol_name','like','%SAL')
+        ->where('pol_status',true)
         ->get();
         return view('livewire.iso-sal.iso-sal-holder')->extends('layouts.main');
     }
--- resources/views/livewire/iso-sal/iso-sal-holder.blade.php
+++ resources/views/livewire/iso-sal/iso-sal-holder.blade.php
@@ -38,7 +38,7 @@
                                     <td style="text-align: center">{{\Carbon\Carbon::parse($item->recipient_date)->format('d/m/Y')}}</td>        
                                     <td style="text-align: center">{{$item->iso_docutype_code}}</td>    
                                     <td>
-                                        <a href="images/isodocuments/{{$item->iso_doculist_filename}}">
+                                        <a href="images/isodocuments/{{$item->iso_doculist_filename}}" target=”_blank”>
                                         {{$item->iso_doculist_name}} ({{$item->iso_doculist_code}})
                                         </a>
                                     </td>   
@@ -49,6 +49,46 @@
                                             <i class="fas fa-edit"></i>
                                         </button>
                                     </td>                                     
+                                </tr>     
+                                @endforeach                                                        
+                           </tbody>
+                        </table>
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </div><hr>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-3">
+                            <h3 class="card-title">แผนภูมิเต่า && ความเสี่ยง</h3>                           
+                        </div>
+                        <div class="col-3"></div>
+                        <div class="col-3"></div>                
+                    </div><hr>                                                      
+                    <div class="table-responsive">
+                        <table id="tb_job" class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th style="text-align: center">วันที่</th>
+                                    <th style="text-align: center">ประเภทเอกสาร</th>
+                                    <th style="text-align: center">เอกสาร</th>
+                                                                                                                
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($policy as $key => $item)
+                                <tr>                                     
+                                    <td style="text-align: center">{{\Carbon\Carbon::parse($item->pol_date)->format('d/m/Y')}}</td>        
+                                    <td>{{$item->pol_type}}</td>    
+                                    <td>
+                                        <a href="images/isopolicy/{{$item->pol_filename}}" target=”_blank”>
+                                        {{$item->pol_name}}
+                                        </a>
+                                    </td>                                       
                                 </tr>     
                                 @endforeach                                                        
                            </tbody>
