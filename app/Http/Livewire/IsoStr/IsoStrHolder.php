f0f5e444a2b00995985ad90753d5bcf5334ca914
NattapongLam <it.9nat@gmail.com>
2023-09-29 11:29:37 +0700

    ok

--- app/Http/Livewire/IsoStr/IsoStrHolder.php
+++ app/Http/Livewire/IsoStr/IsoStrHolder.php
@@ -8,11 +8,20 @@ use Illuminate\Support\Facades\DB;
 class IsoStrHolder extends Component
 {
     public $holder = [];
+    public $policy = [];
+    public $kpi = [];
 
     public function render()
     {
         $this->holder = DB::table('vw_iso_holderperson')
         ->where('emp_department_refcode','STR')
+        ->get();
+        $this->policy = DB::table('iso_policy_lsits')
+        ->where('pol_name','like','%STR%')
+        ->where('pol_status',true)
+        ->get();
+        $this->kpi = DB::table('iso_ict_monthkpis')
+        ->where('dep_name','STR')
         ->get();
         return view('livewire.iso-str.iso-str-holder')->extends('layouts.main');
     }
