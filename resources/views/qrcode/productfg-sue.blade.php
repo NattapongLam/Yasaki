a5a98de74780fcb11fbc96bb2d29aa6b5eeb3dbb
NattapongLam <it.9nat@gmail.com>
2023-10-24 15:25:42 +0700

    ok

--- resources/views/qrcode/productfg-sue.blade.php
+++ resources/views/qrcode/productfg-sue.blade.php
@@ -14,16 +14,16 @@
                                             <div class="col-md-2 col-sm-3 col-4">
                                                 <div class="nav flex-column nav-pills " id="v-pills-tab" role="tablist" aria-orientation="vertical">
                                                     <a class="nav-link active" id="product-1-tab" data-bs-toggle="pill" href="#product-1" role="tab" aria-controls="product-1" aria-selected="true">
-                                                        <img src="assets/images/product/{{$hd->PicFileName1}}" alt="" class="img-fluid mx-auto d-block rounded">
+                                                        <img src="{{asset(assets/images/product/($hd->PicFileName1))}}" alt="" class="img-fluid mx-auto d-block rounded">
                                                     </a>
                                                     <a class="nav-link" id="product-2-tab" data-bs-toggle="pill" href="#product-2" role="tab" aria-controls="product-2" aria-selected="false">
-                                                        <img src="assets/images/product/{{$hd->PicFileName2}}" alt="" class="img-fluid mx-auto d-block rounded">
+                                                        <img src="{{asset(assets/images/product/($hd->PicFileName2))}}" alt="" class="img-fluid mx-auto d-block rounded">
                                                     </a>
                                                     <a class="nav-link" id="product-3-tab" data-bs-toggle="pill" href="#product-3" role="tab" aria-controls="product-3" aria-selected="false">
-                                                        <img src="assets/images/product/{{$hd->PicFileName3}}" alt="" class="img-fluid mx-auto d-block rounded">
+                                                        <img src="{{asset(assets/images/product/($hd->PicFileName3))}}" alt="" class="img-fluid mx-auto d-block rounded">
                                                     </a>
                                                     <a class="nav-link" id="product-4-tab" data-bs-toggle="pill" href="#product-4" role="tab" aria-controls="product-4" aria-selected="false">
-                                                        <img src="assets/images/product/{{$hd->PicFileName4}}" alt="" class="img-fluid mx-auto d-block rounded">
+                                                        <img src="{{asset(assets/images/product/($hd->PicFileName4))}}" alt="" class="img-fluid mx-auto d-block rounded">
                                                     </a>
                                                 </div>
                                             </div>
@@ -31,22 +31,22 @@
                                                 <div class="tab-content" id="v-pills-tabContent">
                                                     <div class="tab-pane fade show active" id="product-1" role="tabpanel" aria-labelledby="product-1-tab">
                                                         <div>
-                                                            <img src="assets/images/product/{{$hd->PicFileName1}}" alt="" class="img-fluid mx-auto d-block">
+                                                            <img src="{{asset(assets/images/product/($hd->PicFileName1))}}" alt="" class="img-fluid mx-auto d-block">
                                                         </div>
                                                     </div>
                                                     <div class="tab-pane fade" id="product-2" role="tabpanel" aria-labelledby="product-2-tab">
                                                         <div>
-                                                            <img src="assets/images/product/{{$hd->PicFileName2}}" alt="" class="img-fluid mx-auto d-block">
+                                                            <img src="{{asset(assets/images/product/($hd->PicFileName2))}}" alt="" class="img-fluid mx-auto d-block">
                                                         </div>
                                                     </div>
                                                     <div class="tab-pane fade" id="product-3" role="tabpanel" aria-labelledby="product-3-tab">
                                                         <div>
-                                                            <img src="assets/images/product/{{$hd->PicFileName3}}" alt="" class="img-fluid mx-auto d-block">
+                                                            <img src="{{asset(assets/images/product/($hd->PicFileName3))}}" alt="" class="img-fluid mx-auto d-block">
                                                         </div>
                                                     </div>
                                                     <div class="tab-pane fade" id="product-4" role="tabpanel" aria-labelledby="product-4-tab">
                                                         <div>
-                                                            <img src="assets/images/product/{{$hd->PicFileName4}}" alt="" class="img-fluid mx-auto d-block">
+                                                            <img src="{{asset(assets/images/product/($hd->PicFileName4))}}" alt="" class="img-fluid mx-auto d-block">
                                                         </div>
                                                     </div>
                                                 </div>
